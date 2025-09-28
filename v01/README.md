d7ea451f1b1f89ae556677a6dd351018b770bc85
Erik Wallerman <erik.wallerman@scania.com>
2025-09-28 22:26:40 +0200

    basic functionality

--- v01/README.md
+++ v01/README.md
@@ -1,2 +1,38 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+# Exekvering
+
+Programmet körs från en prompt i v01 katalogen med kommandot
+
+`dart run bin/v01.dart`
+
+Programmet frågar efter första och andra värdet som kan vara heltal, sedan vilken operation + eller -.
+
+Inmatade argument verifieras så att bara heltal och operationerna + och - är giltiga, om andra argument matas in så frågas igen.
+
+Efter korrekta argument utförs beräkningen och resultatet skrivs ut. 
+
+# Funktions exempel
+
+- Funktioner deklareras med returtyp och void om metoden inte returnerar något.
+- Sedan ett namn på metoden och paranteser efter metodnamnet som innesluter en lista av argument separerade av kommatecken om det finns några argument till metoden. 
+- För varje argument anges type och namn.
+- Metodens logik innesluts i måsvingar, { }.
+- Om något returneras från metoden så görs det med nyckelordet return
+
+### Generiskt exempel
+
+```
+<returTyp> <metodNamn>(<Arg1Datatyp> <arg1Namn>, <Arg2Datatyp> <arg2Namn>) {
+    return <instansAvReturTyp>;
+}
+```
+
+### Konkret exempel
+```
+String whatIs(int arg1, int arg2) {
+    if (arg1 + arg2 == 42) {
+        return "The meaning of life :)";
+    } else {
+        return "Some other stuff";
+    }
+}
+```
--- v01/bin/v01.dart
+++ v01/bin/v01.dart
@@ -1,5 +1,52 @@
-import 'package:v01/v01.dart' as v01;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${v01.calculate()}!');
+  int firstValue = getIntArg('Ange första talet: ');
+  int secondValue = getIntArg('Ange andra talet: ');
+  String operator = getOperator();
+  calculateAndPrintResult(firstValue, secondValue, operator);
+}
+
+int getIntArg(String inputLabel) {
+  while (true) {
+    print(inputLabel);
+    String? readArg = stdin.readLineSync();
+
+    if (readArg != null) {
+      int? val = int.tryParse(readArg);
+
+      if (val != null) {
+        return val;
+      }
+    }
+
+    print('Incorrect argument, please try again!');
+  }
+}
+
+String getOperator() {
+  while (true) {
+    print('Vilken operation vill du göra? (+ eller -): ');
+    String? readArg = stdin.readLineSync();
+
+    if (readArg != null && (readArg == "+" || readArg == "-")) {
+      return readArg;
+    }
+
+    print('Incorrect argument, please try again!');
+  }
+}
+
+void calculateAndPrintResult(int firstValue, int secondValue, String operator) {
+  if (operator == "+") {
+    printResult(firstValue + secondValue);
+  } else if (operator =="-") {
+    printResult(firstValue - secondValue);
+  } else {
+    print("Unsupported operator");
+  }
+}
+
+void printResult(int result) {
+  print("Resultatet är: $result");
 }
